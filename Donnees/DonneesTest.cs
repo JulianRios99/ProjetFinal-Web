416c54206eb23783e375e4eb8df86209a57a2052
PaolaAGM <paorap.03@hotmail.com>
2023-04-20 13:04:08 -0400

    Ajoute Reservations - Données Test

--- Donnees/DonneesTest.cs
+++ Donnees/DonneesTest.cs
@@ -138,22 +138,164 @@ namespace ProjetFinal.Donnees
 
             var ouvrage5 = new Ouvrages()
             {
-                Titre = "Journal d'Hirondelle",
+                Titre = "Acide Sulfurique",
+                Auteur = "Amelie Nothomb",
+                Exemplaires = 2
+
+            };
+
+            var ouvrage6 = new Ouvrages()
+            {
+                Titre = "Le mystère par excellence",
+                Auteur = "Amelie Nothomb",
+                Exemplaires = 2
+
+            };
+
+            var ouvrage7 = new Ouvrages()
+            {
+                Titre = "Le Sabotage amoureux",
+                Auteur = "Amelie Nothomb",
+                Exemplaires = 2
+
+            };
+
+            var ouvrage8 = new Ouvrages()
+            {
+                Titre = "Le voyage d'hiver",
+                Auteur = "Amelie Nothomb",
+                Exemplaires = 2
+
+            };
+
+            var ouvrage9 = new Ouvrages()
+            {
+                Titre = "Les catalinaires",
                 Auteur = "Amelie Nothomb",
                 Exemplaires = 1
 
             };
 
-            //------ continuer les ouvrage (son 22)
 
-            await bibliotheque.Ouvrages.AddRangeAsync(new Ouvrages[] { ouvrage1, ouvrage2, ouvrage3, ouvrage4, ouvrage5 }); // continuar agregando los ouvrages 
+            var ouvrage10 = new Ouvrages()
+            {
+                Titre = "Métaphysique Des Tubes",
+                Auteur = "Amelie Nothomb",
+                Exemplaires = 3
+
+            };
+
+            var ouvrage11 = new Ouvrages()
+            {
+                Titre = "Premier bilan après l’apocalypse",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 1
+
+            };
+
+
+            var ouvrage12 = new Ouvrages()
+            {
+                Titre = "Au secours pardon",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 1
+
+            };
+
+
+
+            var ouvrage13 = new Ouvrages()
+            {
+                Titre = "Un roman français",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 1
+
+            };
+
+            var ouvrage14 = new Ouvrages()
+            {
+                Titre = "Mémoires D'Un Jeune Homme Dérangé ",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 1
+
+            };
+
+
+            var ouvrage15 = new Ouvrages()
+            {
+                Titre = "99 Francs",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 2
+
+            };
+
+            var ouvrage16 = new Ouvrages()
+            {
+                Titre = "L'homme qui pleure de rire",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 2
+
+            };
+
+            var ouvrage17 = new Ouvrages()
+            {
+                Titre = "Vacances dans le coma",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 1
+
+            };
+
+            var ouvrage18 = new Ouvrages()
+            {
+                Titre = "La frivolité est une affaire sérieuse ",
+                Auteur = "Frédéric Beigbeder",
+                Exemplaires = 1
+
+            };
+
+            var ouvrage19 = new Ouvrages()
+            {
+                Titre = "La petite Chose",
+                Auteur = "Alphonse Daudet",
+                Exemplaires = 1
+
+            };
+
+            var ouvrage20 = new Ouvrages()
+            {
+                Titre = "Les lettres de mon moulin",
+                Auteur = "Alphonse Daudet",
+                Exemplaires = 2
+
+            };
+
+            var ouvrage21 = new Ouvrages()
+            {
+                Titre = "Tartarin sur les Alpes",
+                Auteur = "Alphonse Daudet",
+                Exemplaires = 2
+
+            };
+
+
+            var ouvrage22 = new Ouvrages()
+            {
+                Titre = "Tartarin sur les Alpes",
+                Auteur = "Alphonse Daudet",
+                Exemplaires = 2
+
+            };
+
+            
+            await bibliotheque.Ouvrages.AddRangeAsync(new Ouvrages[] { ouvrage1, ouvrage2, ouvrage3, ouvrage4, ouvrage5, ouvrage6, ouvrage7, ouvrage8, ouvrage9, ouvrage10,
+                ouvrage11, ouvrage12, ouvrage13, ouvrage14, ouvrage15, ouvrage16, ouvrage17, ouvrage18, ouvrage19, ouvrage20, ouvrage21, ouvrage22 });
 
 
 
             var reservation1 = new Reservations()
             {
                 Utilisateurs = michel,
-                Ouvrage = ouvrage1  // MODIFIER louvrage correct est la 10 
+                Ouvrage = ouvrage10  // Il manque ajouter lautre 
             };
 
             /*
@@ -166,22 +308,24 @@ namespace ProjetFinal.Donnees
             var reservation3 = new Reservations()
             {
                 Utilisateurs = felix,
-                Ouvrage = ouvrage1 // MODIFIER louvrage correct est la 6
+                Ouvrage = ouvrage6
             };
 
 
             var reservation4 = new Reservations()
             {
                 Utilisateurs = anais,
-                Ouvrage = ouvrage4
-               
+                Ouvrage = ouvrage4    
             };
 
 
             var reservation5 = new Reservations()
             {
                 Utilisateurs = kinza,
-                Ouvrage = ouvrage3 // MODIFIER louvrage correct est la 6
+
+              //  Ouvrage = List<Ouvrages>(ouvrage20, ouvrage3)
+
+                Ouvrage = ouvrage20 // il manque ajouter l'ouvrage3 
             };
 
             await bibliotheque.Reservations.AddRangeAsync(new Reservations[] { reservation1, reservation3, reservation4, reservation5 }); // MODIFIER LES RESERVATIONS
